a18ef88e42cce1ec41937a3b02ed8fbbc9696729
Diane10 <46956585+Diane10@users.noreply.github.com>
2020-09-26 18:27:30 +0200

    Add files via upload

---
+++ assets/js/readings.js
@@ -0,0 +1,34 @@
+// functions
+const add_contact =async()=>{
+    const names = document.getElementById('names').value
+    const emails = document.getElementById('emails').value
+    const subjects = document.getElementById('subjects').value
+    const messages = document.getElementById('messages').value    
+        const response = await fetch('http://rlsgldiane2020.herokuapp.com/contact/',{
+            method:"POST",
+            body:JSON.stringify({
+                name:names,
+                email:emails,
+                subject:subjects,
+                message:messages
+
+            }),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+
+        const responseData = await response.json()
+        console.log(responseData)
+        if(response.ok){
+            location.href="../../index.html"
+        }else{
+            alert(responseData.error)
+        }
+    
+}
+
+
+// events
+
+document.getElementById('submit').addEventListener('click',add_contact)
---
+++ assets/js/subscribe.js
@@ -0,0 +1,29 @@
+// functions
+const subscribe =async()=>{
+    const emails = document.getElementById('emails').value
+        const response = await fetch('http://rlsgldiane2020.herokuapp.com/subscribe/',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    email:emails
+                
+
+            }),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+
+        const responseData = await response.json()
+        console.log(responseData)
+        if(response.ok){
+            location.href="../../contact.html"
+        }else{
+            alert(responseData.error)
+        }
+}
+
+
+// events
+
+document.getElementById('subscribe').addEventListener('click',subscribe)
