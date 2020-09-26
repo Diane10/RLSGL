66dec9e5e84ed151f63ef1f45aace4b38cdaa9c8
Diane10 <46956585+Diane10@users.noreply.github.com>
2020-09-26 19:13:38 +0200

    Update readings.js

--- assets/js/readings.js
+++ assets/js/readings.js
@@ -4,7 +4,7 @@ const add_contact =async()=>{
     const emails = document.getElementById('emails').value
     const subjects = document.getElementById('subjects').value
     const messages = document.getElementById('messages').value    
-        const response = await fetch('http://rlsgldiane2020.herokuapp.com/contact/',{
+        const response = await fetch('https://rlsgldiane2020.herokuapp.com/contact/',{
             method:"POST",
             body:JSON.stringify({
                 name:names,
