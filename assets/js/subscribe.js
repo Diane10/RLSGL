bc3c23a512053a07859bdfaef2b66a1d30b09704
Diane10 <46956585+Diane10@users.noreply.github.com>
2020-09-27 22:30:29 +0200

    Update subscribe.js

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,13 +1,13 @@
 // functions
 const subscribe =async()=>{
-    const emails = document.getElementById('emails').value
+    const emails = document.getElementById('emailsub').value
+    console.log(emails)
         const response = await fetch('https://rlsgldiane2020.herokuapp.com/subscribe/',{
             method:"POST",
             body:JSON.stringify(
                 {
                     email:emails
                 
-
             }),
             headers:{
                 "Content-Type": "application/json"
@@ -17,13 +17,14 @@ const subscribe =async()=>{
         const responseData = await response.json()
         console.log(responseData)
         if(response.ok){
-            location.href="../../contact.html"
+            alert("Thank you!!!")
         }else{
-            alert(responseData.error)
+            alert(responseData.data)
         }
+    
 }
 
 
 // events
 
-document.getElementById('subscribe').addEventListener('click',subscribe)
+document.getElementById('sub').addEventListener('click',subscribe)
